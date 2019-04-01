19e34d118d3f43adc8010a82cbc4ed4bf135f4b0
sarajoha <sjcc333@gmail.com>
2019-04-01 11:33:20 -0500

    Added docstring

---
+++ prueba.py
@@ -0,0 +1,9 @@
+"""
+Pruebla de Monoku - En Monoku compran muchas galguerias y muchas veces hay que
+botarlas porque se vencen. Por eso este programa responde las siguientes preguntas:
+- Qué come cada miembro del equipo?
+- El nombre de la persona que más consumió
+- Nombre del producto que más se consumió.
+"""
+
+galguerias: {}
