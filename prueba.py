1562d228ee643c92ae059d8eb2b923d5e6aba430
sarajoha <sjcc333@gmail.com>
2019-04-01 12:27:40 -0500

    Created nested dictionary

--- prueba.py
+++ prueba.py
@@ -6,4 +6,31 @@ botarlas porque se vencen. Por eso este programa responde las siguientes pregunt
 - Nombre del producto que más se consumió.
 """
 
-galguerias: {}
+#Datos del consumo mensual de galguerias (chucherias)
+galguerias: {"Leonel": {"Cocosette": 4, "Cub social": 0, "Ritz": 0, "Jumbo":0,
+                        "Jet":0, "Pringles":0, "Chips Ahoy":0, "Palomitas":0,
+                        "Tosh": 0, "Bridge":0, "Bon Bon Bum":0, "Frunas": 0, 
+                        "Saltin Noel":0, "Gansito":0, "Chocorramos":0, "Gala Miti":0,
+                        "Achiras":0,
+                        },
+            "Jose": {"Cocosette": 2},
+            "Julian": {},
+            "Juan": {},
+            "Luis": {},
+            "Yeison": {},
+            "Andres C": {},
+            "Eli": {},
+            "Alejandra": {},
+            "Estefany": {},
+            "Brian": {},
+            "Natalia": {},
+            "Luix": {},
+            "Andres A": {}
+            }
+
+
+#Funcion que imprima lo que come cada persona, ignorando valores de 0
+
+#Funcion que retorne nombre de la persona que mas consumio (suma de todos los valores)
+
+#Funcion que retorne producto que mas se consumio
