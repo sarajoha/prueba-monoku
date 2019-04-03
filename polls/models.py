a2371eb1ba84c3fbfbe137bcf3861b5692bfff0b
sarajoha <sjcc333@gmail.com>
2019-04-03 10:00:02 -0500

    Fixed Team_member method

--- polls/models.py
+++ polls/models.py
@@ -1,24 +1,27 @@
 from django.db import models
 from django.utils import timezone
 # Create your models here.
-#poner todo en ingles!! 
+#poner todo en ingles!!
 
 class Team_member(models.Model):
     name = models.CharField(max_length=200)
-    CONSUME = []
-    PRODUCTOS = []
-    consumo_total = models.CharField(max_length=200, default=CONSUME)
-    consumo_productos = models.CharField(max_length=200, default=PRODUCTOS)
+    consumo_total = models.CharField(max_length=500)
+    consumo_productos = models.CharField(max_length=500)
 
-    def consumir(self, producto, cantidad):
+    def consumir(self, consumo):
         #metodo para consumir producto, nombre producto y nro de veces
-        if int(cantidad):
-            CONSUME.append((producto, cantidad))
-        else:
-            return "Valores no validos"
+        productos = []
+        for tup in consumo:
+            if int(tup[1]):
+                continue
+            if tup[1] > 0:
+                productos.append(tup[0])
 
-        if cantidad > 0:
-            PRODUCTOS.append(producto)
+        consumo_total = consumo
+        consumo_productos = productos
+
+    def __str__(self):
+        return self.name
 
 
 class Producto(models.Model):
