c54742c8447f1401f70f8c45513eb26d6ae68849
sarajoha <sjcc333@gmail.com>
2019-04-03 10:06:03 -0500

    Changed variables to english

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Team_member, Producto
+from .models import Team_member, Product
 
 
 # Register your models here.
 admin.site.register(Team_member)
-admin.site.register(Producto)
+admin.site.register(Product)
--- polls/models.py
+++ polls/models.py
@@ -1,30 +1,30 @@
 from django.db import models
 from django.utils import timezone
 # Create your models here.
-#poner todo en ingles!!
+
 
 class Team_member(models.Model):
     name = models.CharField(max_length=200)
-    consumo_total = models.CharField(max_length=500)
-    consumo_productos = models.CharField(max_length=500)
+    consumption = models.CharField(max_length=500)
+    consumed_products = models.CharField(max_length=500)
 
-    def consumir(self, consumo):
+    def consume(self, consumed):
         #metodo para consumir producto, nombre producto y nro de veces
-        productos = []
-        for tup in consumo:
+        products = []
+        for tup in consumed:
             if int(tup[1]):
                 continue
             if tup[1] > 0:
-                productos.append(tup[0])
+                products.append(tup[0])
 
-        consumo_total = consumo
-        consumo_productos = productos
+        consumption = consumed
+        consumed_products = products
 
     def __str__(self):
         return self.name
 
 
-class Producto(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
